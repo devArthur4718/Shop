f2499ab925c0621f25e8c13f26a8ca6361f6c66a
Arthur Gomes <devarthur4718@gmail.com>
2020-04-06 13:46:04 -0300

    camera permission

--- app/src/main/java/com/stetter/escambo/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/stetter/escambo/ui/splash/SplashActivity.kt
@@ -1,12 +1,20 @@
 package com.stetter.escambo.ui.splash
 
+import android.Manifest
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.single.PermissionListener
 import com.stetter.escambo.BuildConfig
 import com.stetter.escambo.R
 import com.stetter.escambo.ui.login.LoginActivity
+
 
 class SplashActivity : AppCompatActivity() {
 
@@ -19,6 +27,29 @@ class SplashActivity : AppCompatActivity() {
     }
 
     private fun initViews() {
+
+        Dexter.withActivity(this)
+            .withPermission(Manifest.permission.CAMERA)
+            .withListener(object : PermissionListener {
+                override fun onPermissionGranted(response: PermissionGrantedResponse) {
+                    openLoginActivity()
+                }
+
+                override fun onPermissionDenied(response: PermissionDeniedResponse) {
+                    openLoginActivity()
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permission: PermissionRequest?,
+                    token: PermissionToken?
+                ) { /* ... */
+                }
+            }).check()
+
+
+    }
+
+    private fun openLoginActivity() {
         Handler().postDelayed({
             startActivity(
                 Intent(this,
