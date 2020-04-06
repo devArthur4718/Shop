64b27f2cda3cbc09b474a913990025a04bab3b81
Arthur Gomes <devarthur4718@gmail.com>
2020-04-06 20:59:59 -0300

    edit profile

---
+++ app/src/main/java/com/stetter/escambo/ui/core/profile/ProfileDetail.kt
@@ -0,0 +1,14 @@
+package com.stetter.escambo.ui.core.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.stetter.escambo.R
+
+class ProfileDetail : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_detail)
+    }
+}
--- app/src/main/java/com/stetter/escambo/ui/core/profile/ProfileViewModel.kt
+++ app/src/main/java/com/stetter/escambo/ui/core/profile/ProfileViewModel.kt
@@ -1,7 +1,28 @@
 package com.stetter.escambo.ui.core.profile
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.stetter.escambo.net.models.Product
 
 class ProfileViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val _listProduct = MutableLiveData<List<Product>>()
+    val listProduct : LiveData<List<Product>> get() = _listProduct
+
+    init {
+        var dummyProduct = listOf(
+            Product(""),
+            Product(""),
+            Product(""),
+            Product(""),
+            Product(""),
+            Product(""),
+            Product(""),
+            Product("")
+        )
+        _listProduct.value = dummyProduct
+    }
+
+
 }
