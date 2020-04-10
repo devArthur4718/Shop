e5151c3b6eabe680b49582cb2e09af2969067703
Arthur Gomes <devarthur4718@gmail.com>
2020-04-09 22:38:37 -0300

    toogle input states

---
+++ app/src/main/java/com/stetter/escambo/extension/ContextExtensions.kt
@@ -0,0 +1,18 @@
+package com.stetter.escambo.extension
+
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+
+
+fun Context.showKeyboard() {
+    val imm: InputMethodManager =
+        getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0)
+}
+
+fun Context.hideKeyBoard(view : View){
+    val imm =
+        getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
+    imm!!.hideSoftInputFromWindow(view.getWindowToken(), 0)
+}
--- app/src/main/java/com/stetter/escambo/ui/core/profile/ProfileDetail.kt
+++ app/src/main/java/com/stetter/escambo/ui/core/profile/ProfileDetail.kt
@@ -1,21 +1,30 @@
 package com.stetter.escambo.ui.core.profile
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.UserProfileChangeRequest
 import com.google.firebase.storage.FirebaseStorage
 import com.stetter.escambo.R
 import com.stetter.escambo.databinding.ActivityProfileDetailBinding
 import com.stetter.escambo.extension.CircularProgress
+import com.stetter.escambo.extension.hideKeyBoard
+import com.stetter.escambo.extension.showKeyboard
 import com.stetter.escambo.extension.showPickImageProfile
 import com.stetter.escambo.glide.GlideApp
 import com.stetter.escambo.net.models.RegisterUser
@@ -24,10 +33,11 @@ import com.stetter.escambo.ui.core.add.AddProduct
 import com.stetter.escambo.ui.login.LoginActivity
 import java.util.*
 
+
 class ProfileDetail : BaseActivity() {
 
     private lateinit var binding: ActivityProfileDetailBinding
-    private lateinit var viewmodel :UpdateProfileViewModel
+    private lateinit var viewmodel: UpdateProfileViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,7 +56,8 @@ class ProfileDetail : BaseActivity() {
         binding.ivLogout.setOnClickListener {
             viewmodel.logout()
             var intent = Intent(this, LoginActivity::class.java)
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                .addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
             startActivity(intent)
         }
 
@@ -68,7 +79,7 @@ class ProfileDetail : BaseActivity() {
                 .build()
 
             user?.updateProfile(profileUpdates)
-                ?.addOnCompleteListener {task ->
+                ?.addOnCompleteListener { task ->
 
                     if (task.isSuccessful) {
                         Log.d("ProfileDetail", "User profile updated.")
@@ -84,37 +95,40 @@ class ProfileDetail : BaseActivity() {
 
     private fun onProfileImageReceived(imgUrl: String) {
         val storage = FirebaseStorage.getInstance()
-        if(imgUrl.length > 1){
-            val gsReference = storage.getReferenceFromUrl("gs://escambo-1b51d.appspot.com/${imgUrl}")
+        if (imgUrl.length > 1) {
+            val gsReference =
+                storage.getReferenceFromUrl("gs://escambo-1b51d.appspot.com/${imgUrl}")
             GlideApp.with(this)
                 .load(gsReference)
                 .placeholder(this?.CircularProgress())
                 .into(binding.ivDetailProfileImage)
 
-        }else{
+        } else {
             binding.ivDetailProfileImage.setImageDrawable(resources.getDrawable(R.drawable.ic_young))
         }
     }
 
     private fun onPickImageIntent(it: Boolean?) {
         it?.let {
-            if(it){
+            if (it) {
                 pickImage()
             }
         }
     }
+
     private fun pickImage() {
         val intent = Intent(Intent.ACTION_PICK)
         intent.type = "image/*"
         startActivityForResult(intent, AddProduct.RQ_PICK_FROM_GALLERY)
     }
+
     companion object {
         fun newInstance() = AddProduct()
         const val RQ_PICK_PHOTO = 0
         const val RQ_TAKE_PHOTO = 1
     }
 
-    var selectedPhotoUri : Uri? = null
+    var selectedPhotoUri: Uri? = null
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
@@ -139,10 +153,10 @@ class ProfileDetail : BaseActivity() {
     }
 
     private fun sendImageToFirebase(selectedPhotoUri: Uri?) {
-            val filename = UUID.randomUUID().toString()
-            selectedPhotoUri?.let { uri ->
-                viewmodel.uploadImageToFirebase(filename, uri)
-            }
+        val filename = UUID.randomUUID().toString()
+        selectedPhotoUri?.let { uri ->
+            viewmodel.uploadImageToFirebase(filename, uri)
+        }
 
     }
 
@@ -162,17 +176,42 @@ class ProfileDetail : BaseActivity() {
 
         //Load user photo
         val storage = FirebaseStorage.getInstance()
-        if(userdata!!.photoUrl.length > 1){
-            val gsReference = storage.getReferenceFromUrl("gs://escambo-1b51d.appspot.com${userdata.photoUrl}")
+        if (userdata!!.photoUrl.length > 1) {
+            val gsReference =
+                storage.getReferenceFromUrl("gs://escambo-1b51d.appspot.com${userdata.photoUrl}")
             GlideApp.with(this)
                 .load(gsReference)
                 .placeholder(this?.CircularProgress())
                 .into(binding.ivDetailProfileImage)
 
-        }else{
+        } else {
             binding.ivDetailProfileImage.setImageDrawable(resources.getDrawable(R.drawable.ic_young))
         }
     }
 
 
+    fun onEditClick(view: View) {
+        when (view.id) {
+            R.id.tvEditFullName -> toogleInput(binding.inputFullName, binding.tvEditFullName)
+            R.id.tvEditEmail -> toogleInput(binding.inputEmail, binding.tvEditEmail)
+            R.id.tvEditPassword -> toogleInput(binding.inputPassword,binding.tvEditPassword)
+            R.id.tvEditBirthDate -> toogleInput(binding.inputBirthDate, binding.tvEditBirthDate)
+            R.id.tvEditPostalCode -> toogleInput(binding.inputPostalCode, binding.tvEditPostalCode)
+            R.id.tvEditUF -> toogleInput(binding.inputUF, binding.tvEditUF)
+            R.id.tvEditCity -> toogleInput(binding.inputCity, binding.tvEditCity)
+        }
+    }
+
+    fun toogleInput(view : TextInputLayout, textEditLabel : TextView){
+        view.editText!!.isEnabled = !view.editText!!.isEnabled
+        view.editText!!.requestFocus()
+        view.editText!!.setSelection(view.editText!!.text.toString().length)
+        if(view.editText!!.isEnabled){
+            showKeyboard()
+            textEditLabel.text = getString(R.string.save_it)
+        }else{
+            textEditLabel.text = getString(R.string.edit)
+            hideKeyBoard(view)
+        }
+    }
 }
