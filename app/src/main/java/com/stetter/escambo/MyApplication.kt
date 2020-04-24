d8603186277f5630c9dc3425bab777a4678d2326
Arthur Gomes <devarthur4718@gmail.com>
2020-04-23 21:15:29 -0300

    added one signal

--- app/src/main/java/com/stetter/escambo/MyApplication.kt
+++ app/src/main/java/com/stetter/escambo/MyApplication.kt
@@ -6,6 +6,7 @@ import androidx.camera.core.CameraXConfig
 import com.facebook.FacebookSdk
 import com.facebook.appevents.AppEventsLogger
 import com.facebook.stetho.Stetho
+import com.onesignal.OneSignal;
 
 class MyApplication: Application(), CameraXConfig.Provider {
     override fun onCreate() {
@@ -13,6 +14,12 @@ class MyApplication: Application(), CameraXConfig.Provider {
         Stetho.initializeWithDefaults(this)
 //        FacebookSdk.sdkInitialize(getApplicationContext());
         AppEventsLogger.activateApp(this);
+
+        // OneSignal Initialization
+        OneSignal.startInit(this)
+            .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
+            .unsubscribeWhenNotificationsAreDisabled(true)
+            .init();
     }
 
     override fun getCameraXConfig(): CameraXConfig {
